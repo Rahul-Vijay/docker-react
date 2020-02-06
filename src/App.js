bf708b2a5ed8825a5c6c90902670c3e10635d861
Rahul_Vijay <vk.rahul318@gmail.com>
2020-02-06 15:04:48 +0530

    changed p tag

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed
+          Docker is cool
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Docker is cool
+          Learn react
         </a>
       </header>
     </div>
